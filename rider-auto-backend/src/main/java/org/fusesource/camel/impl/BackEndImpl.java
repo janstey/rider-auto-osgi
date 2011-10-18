3c60190b6148ed61a66f4cbedb60f54f5a5a2249
Jonathan Anstey <janstey@gmail.com>
2011-10-18 14:34:13 -0230

    Further changes that I missed..

--- rider-auto-backend/src/main/java/org/fusesource/camel/impl/BackEndImpl.java
+++ rider-auto-backend/src/main/java/org/fusesource/camel/impl/BackEndImpl.java
@@ -1,9 +1,5 @@
 package org.fusesource.camel.impl;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.fusesource.camel.model.Order;
